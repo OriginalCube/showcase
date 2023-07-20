7e3092d73a7bec05da77eed39733f54d9694ae16
lancemiraflorcube@gmail.com <lancemiraflorcube@gmail.com>
2023-07-20 22:14:39 +0800

    Added genshin layout

--- src/components/homepage/Catalouge.tsx
+++ src/components/homepage/Catalouge.tsx
@@ -6,15 +6,6 @@ import Genshin from "./Genshin";
 const Catalouge = () => {
   return (
     <div className="h-auto w-full relative text-gray-800">
-      <motion.div
-        initial={{ y: "50vh" }}
-        animate={{ y: 0 }}
-        transition={{ delay: 0.5, duration: 1 }}
-        className="homepage-waves h-full"
-        style={{
-          backgroundImage: `url(/assets/images/layered-waves-haikei_1.svg)`,
-        }}
-      ></motion.div>
       <div className="w-full h-auto">
         <p className="text-center pt-80 pb-24 text-8xl">
           Wallpaper Engine Works
--- src/components/homepage/Genshin.tsx
+++ src/components/homepage/Genshin.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const Genshin = () => {
-  return <div className="h-screen w-full"></div>;
+  return (
+    <div
+      className="w-full mt-10"
+      style={{
+        backgroundImage: `url(/assets/genshin/images/wallpaper-default.jpg)`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        height: "200vh",
+      }}
+    ></div>
+  );
 };
 
 export default Genshin;
