0d2e169d8b71888d7067b6470a8645daae8b19e4
lancemiraflorcube@gmail.com <lancemiraflorcube@gmail.com>
2023-08-16 09:35:34 +0800

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Homepage from "./pages/Homepage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Analytics />
         <Routes>
           <Route index element={<Homepage />} />
         </Routes>
